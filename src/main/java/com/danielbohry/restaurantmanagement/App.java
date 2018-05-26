8d5a398f4df6e27fbaffd68eeba50412e1f42ff8
Daniel Bohry <danielbohry@gmail.com>
2018-05-26 13:43:30 -0300

    oauth configs

--- src/main/java/com/danielbohry/restaurantmanagement/App.java
+++ src/main/java/com/danielbohry/restaurantmanagement/App.java
@@ -1,33 +1,32 @@
 package com.danielbohry.restaurantmanagement;
 
-import com.danielbohry.restaurantmanagement.entities.User;
-import com.danielbohry.restaurantmanagement.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
-import java.util.Arrays;
-
-@SpringBootApplication
 @EnableOAuth2Sso
-public class App {
+@SpringBootApplication
+public class App extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 
-    @Autowired
-    public void authenticationManager(AuthenticationManagerBuilder builder, UserRepository repo) throws Exception {
-        if (repo.count() == 0)
-            repo.save(new User(
-                    "admin",
-                    "admin",
-                    new String[]{"USER", "ADMIN"}));
-
-        builder.userDetailsService(username -> new CostomUserDetails(repo.findByUsername(username)));
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/api/restaurant", "/login", "/webjars/**").permitAll()
+                .antMatchers("/api/**").authenticated()
+//                .anyRequest().authenticated()
+                .and().logout().logoutSuccessUrl("/").permitAll()
+//                .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+        ;
     }
 
 }
--- src/main/java/com/danielbohry/restaurantmanagement/controllers/RestaurantController.java
+++ src/main/java/com/danielbohry/restaurantmanagement/controllers/RestaurantController.java
@@ -1,17 +1,20 @@
 package com.danielbohry.restaurantmanagement.controllers;
 
-import com.danielbohry.restaurantmanagement.services.RestaurantService;
 import com.danielbohry.restaurantmanagement.entities.Restaurant;
+import com.danielbohry.restaurantmanagement.services.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/restaurant")
 public class RestaurantController {
 
     private RestaurantService service;
@@ -21,34 +24,33 @@ public class RestaurantController {
         this.service = service;
     }
 
-    @GetMapping("/restaurant")
+    @GetMapping
     public ResponseEntity<List<Restaurant>> getAll() {
         List<Restaurant> response = service.getAll();
         return ResponseEntity.ok().body(response);
     }
 
-    @GetMapping("management/restaurant/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Restaurant> get(@PathVariable("id") String id) {
         Optional<Restaurant> response = service.get(id);
         return response.map(r -> ResponseEntity.ok().body(r))
                 .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
-    @PostMapping("management/restaurant")
-    public ResponseEntity<Restaurant> save(@PathParam("access_token") String token,
-                                           @RequestBody Restaurant restaurant) {
+    @PostMapping
+    public ResponseEntity<Restaurant> save(@RequestBody Restaurant restaurant) {
         Restaurant response = service.save(restaurant);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
-    @PutMapping("management/restaurant/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<Restaurant> update(@PathVariable("id") String id,
                                              @RequestBody Restaurant restaurant) {
         Restaurant response = service.update(id, restaurant);
         return ResponseEntity.ok().body(response);
     }
 
-    @DeleteMapping("management/restaurant/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable("id") String id) {
         service.delete(id);
         return ResponseEntity.ok().build();
