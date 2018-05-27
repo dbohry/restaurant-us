c60daf4a79612a7f677c54ab4c01872fa632589d
Daniel Bohry <danielbohry@gmail.com>
2018-05-27 14:05:11 -0300

    Restaurant constructor and removing set methods

--- src/main/java/com/danielbohry/restaurantmanagement/entities/Restaurant.java
+++ src/main/java/com/danielbohry/restaurantmanagement/entities/Restaurant.java
@@ -23,16 +23,8 @@ public class Restaurant {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     @Override
